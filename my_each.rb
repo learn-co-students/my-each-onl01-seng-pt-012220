6e20e5d62aa357f6aba42fc408d97243e4c0fa87
Lakeisha McCree <lmccree215@icloud.com>
2020-02-04 00:50:22 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,18 @@
-def my_each # put argument(s) here
-  # code here
+list = ["ab", "cd", "ef"]
+
+def my_each(array)
+  i = 0
+  while i < array.length
+    yield(array[i])
+    i = i + 1
+  end
+end
+
+def my_each(array)
+  i = 0
+  while i < array.length
+    yield(array[i])
+    i += 1
+  end
+  array
 end
